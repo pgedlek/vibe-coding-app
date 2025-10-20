11f44b1c241d9d5b78b10374192a98a8c23e67ec
Pawel Gedlek <Pawel.Gedlek@sabre.com>
2025-10-20 15:16:54 +0200

    Init

---
+++ src/main/java/com/pgedlek/vibe_coding/VibeCodingApplication.java
@@ -0,0 +1,13 @@
+package com.pgedlek.vibe_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeCodingApplication.class, args);
+	}
+
+}
